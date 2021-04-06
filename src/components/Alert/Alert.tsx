7850feb361551dff32443b5cac1e70543377263e
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2021-04-06 18:46:26 +0100

    merge

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -22,15 +22,11 @@ export default (props: AlertProps) => {
     const [unmount, setUnmount] = useState(false);
 
     const onTransitionEnd = (e: any) => {
-        console.log(e);
-        if (!show && shouldUnMountOnExit) {
-            setUnmount(true);
-        }
+            if (!show && shouldUnMountOnExit) setUnmount(true)
     };
 
     useEffect(() => {
         setUnmount(false);
-
     }, [show])
     return (
         <React.Fragment>
@@ -48,11 +44,18 @@ interface ClosableAlertProps extends AlertProps {
 }
 const ClosableAlert = (props: ClosableAlertProps) => {
     const { onDismiss, banner, dismissible, type, delay, title, message, action, buttonTitle } = props;
+    const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>();
     const btnTitle = buttonTitle || "OK";
-    const close = () => onDismiss(false);
-    if (delay) {
-        setTimeout(close, delay)
+    const close = () => {
+        if (timeoutId) clearTimeout(timeoutId);
+        onDismiss(false);
     }
+
+    useEffect(() => {
+        if (delay) {
+            setTimeoutId(setTimeout(close, delay));
+        }
+    }, [])
     const style = `alert ${type} ${banner ? 'banner' : ''} ${dismissible ? 'dismissible' : ''}`;
     return (
         <div className={style}>
